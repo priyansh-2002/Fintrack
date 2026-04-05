a9749047db7962d0db5172f8b2e02587a0f2ad60
priyansh-2002 <priyansh.hss02@gmail.com>
2026-04-06 00:28:30 +0530

    fix: deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
